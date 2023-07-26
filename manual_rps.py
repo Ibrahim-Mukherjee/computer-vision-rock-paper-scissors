9c043954ee958d8f4f66e7710c0e3403773b4f75
Ibrahim-Mukherjee <35773504+Ibrahim-Mukherjee@users.noreply.github.com>
2023-07-26 01:01:51 +0100

    Add files via upload

--- manual_rps.py
+++ manual_rps.py
@@ -17,20 +17,47 @@ user_choice = get_user_choice()
 print("Computer chose:", computer_choice)
 print("You chose:", user_choice)
 
-if user_choice == computer_choice:
-  print("It's a tie!")
-elif user_choice == "Rock":
-  if computer_choice == "Scissors":
-    print("Rock smashes scissors! You win!")
-  else:
-    print("Paper covers rock! You lose.")
-elif user_choice == "Paper":
-  if computer_choice == "Rock":
-    print("Paper covers rock! You win!")
-  else:
-    print("Scissors cuts paper! You lose.")
-elif user_choice == "Scissors":
-  if computer_choice == "Paper":
-    print("Scissors cuts paper! You win!")
-  else:
-    print("Rock smashes scissors! You lose.")
+def determine_winner(user_choice, computer_choice):
+
+  if user_choice == computer_choice:
+    print("It's a tie!")
+    winner = None
+
+  elif user_choice == "Rock":
+    if computer_choice == "Scissors":
+      print("Rock smashes scissors! You win!")
+      winner = "User"
+    else:
+      print("Paper covers rock! You lose.")  
+      winner = "Computer"
+
+  elif user_choice == "Paper":
+    if computer_choice == "Rock":
+      print("Paper covers rock! You win!")
+      winner = "User"
+    else:
+      print("Scissors cuts paper! You lose.")
+      winner = "Computer"
+
+  elif user_choice == "Scissors":
+    if computer_choice == "Paper":
+      print("Scissors cuts paper! You win!")
+      winner = "User"
+    else:
+      print("Rock smashes scissors! You lose.")
+      winner = "Computer"
+  
+  return winner
+
+def play():
+
+  print("Let's play Rock Paper Scissors!")
+
+  computer_choice = get_computer_choice()
+  user_choice = get_user_choice()
+
+  winner = determine_winner(user_choice, computer_choice)
+
+  print(winner)
+
+play()
